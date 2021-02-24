7ab17ba181c431a69a80e49efcf11c832982d37b
Kinsuk Kumar <kinsuk.kumar@gmail.com>
2021-02-24 19:51:28 +0530

    first commit

---
+++ BackgroundTask.Demo/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+
+namespace BackgroundTask.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"./App_Data/LogFile.txt")
+                .CreateLogger();
+
+            try 
+            {
+                Log.Information("Starting up the service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "There was a problem starting the service");
+                return;
+            }
+
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).ConfigureServices(services => 
+                    services.AddHostedService<DerivedBackgroundPrinter>())
+            .UseSerilog();
+    }
+}
